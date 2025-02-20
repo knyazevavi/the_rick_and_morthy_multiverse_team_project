c54e11684fb19625426781fef632978f32d8aa8b
mplx <mishaplxx@gmail.com>
2025-02-20 23:57:18 +0300

    feat: implement router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { Home } from "./pages/Home";
-
 export const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='signin'  element={<>sign-in</>} />
+          <Route path='signup'  element={<>signup</>} />
+          <Route path='favorite'  element={<>favorite</>} />
+          <Route path='search'  element={<>search</>} />
+          <Route path='history'  element={<>history</>} />
         </Route>
       </Routes>
     </Router>
