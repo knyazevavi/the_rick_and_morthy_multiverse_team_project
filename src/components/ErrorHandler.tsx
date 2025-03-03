6e47dec868d715cc0aeffd5a1369936e3eabfbd7
Chernov Ilya <ilyacheirnov@gmail.com>
2025-03-03 22:15:38 +0300

    refactor(CharacterPage): Added 404 constant

--- src/components/ErrorHandler.tsx
+++ src/components/ErrorHandler.tsx
@@ -1,13 +1,14 @@
 import { Typography } from "@mui/material";
 import { Navigate } from "react-router-dom";
 
+import { ERROR_404_URL } from "../shared/constants/constants";
 import { ErrorHandlerProps } from "../shared/types/types";
 
 export const ErrorHandler = ({ error }: ErrorHandlerProps) => {
   const apiError = error as { status?: number };
 
   if (apiError?.status === 404) {
-    return <Navigate to="/404" replace />;
+    return <Navigate to={ERROR_404_URL} replace />;
   }
   return <Typography variant="h6"> Something went wrong.</Typography>;
 };
