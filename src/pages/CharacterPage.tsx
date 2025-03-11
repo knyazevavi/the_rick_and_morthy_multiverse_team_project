db1841b1f2ac3a9fe1db0f43fd30ba8673dda4fa
Chernov Ilya <ilyacheirnov@gmail.com>
2025-03-12 00:14:32 +0300

    Added Favorite button

--- src/pages/CharacterPage.tsx
+++ src/pages/CharacterPage.tsx
@@ -1,13 +1,21 @@
-import { Container } from "@mui/material";
+import { Container, Button } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import { useGetCharacterByIdQuery } from "../api/characterApi";
 import { CharacterDetails } from "../components/CharacterDetails";
 import { ErrorHandler } from "../components/ErrorHandler";
 import { Loader } from "../components/Loader";
+import { toggleFavorite } from "../store/favoriteSlice.ts";
+import { RootState } from "../store/store.ts";
 
 export const CharacterPage = () => {
   const { id } = useParams<{ id: string }>();
+  const dispatch = useDispatch();
+
+  const favorites = useSelector(
+    (state: RootState) => state.favorites.favorites,
+  );
 
   const {
     data: character,
@@ -15,10 +23,25 @@ export const CharacterPage = () => {
     error,
   } = useGetCharacterByIdQuery(id ?? "");
 
+  const isFavorite = character ? favorites.includes(character?.id) : false;
+
+  const handleFavoriteButton = () => {
+    if (character) {
+      dispatch(toggleFavorite(character.id));
+    }
+  };
+
   const content =
     (isLoading && <Loader />) ||
     (error && <ErrorHandler error={error} />) ||
-    (character && <CharacterDetails character={character} />);
+    (character && (
+      <>
+        <CharacterDetails character={character} />
+        <Button onClick={handleFavoriteButton}>
+          {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+        </Button>
+      </>
+    ));
 
   return <Container>{content}</Container>;
 };
